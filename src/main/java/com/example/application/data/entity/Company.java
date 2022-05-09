9dd5af4ba8b473b9bdb48877b17a5d00fdb7aa4c
songHyeongwon <songhyeongwon@gmail.com>
2022-05-09 09:05:12 +0900

    수정푸쉬

--- src/main/java/com/example/application/data/entity/Company.java
+++ src/main/java/com/example/application/data/entity/Company.java
@@ -1,18 +1,29 @@
 package com.example.application.data.entity;
 
-import com.example.application.data.AbstractEntity;
 import java.util.LinkedList;
 import java.util.List;
+
 import javax.annotation.Nullable;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.Formula;
+
+import com.example.application.data.AbstractEntity;
 
 @Entity
 public class Company extends AbstractEntity {
     @NotBlank
     private String name;
 
+    @Formula("(select count(c.id) from Contact c where c.company_id = id)") 
+    private int employeeCount;
+
+    public int getEmployeeCount(){
+        return employeeCount;
+    }
+    
     @OneToMany(mappedBy = "company")
     @Nullable
     private List<Contact> employees = new LinkedList<>();
